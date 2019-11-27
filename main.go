bb2cf87b84cb5922c26ecdedd6ae52bb4f145d52
puneeth2001 <puneethchanda2001@gmail.com>
2019-11-27 14:45:33 +0530

    fetch wikidata url

--- main.go
+++ main.go
@@ -8,19 +8,23 @@ import (
 
 func main() {
 	// Instantiate default collector
+	var strings [8]string
+
 	c := colly.NewCollector(
 		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
 		colly.AllowedDomains("en.wikipedia.org"),
 	)
 
 	// On every a element which has href attribute call callback
-	c.OnHTML("th a[href]", func(e *colly.HTMLElement) {
+	c.OnHTML("table tbody tr th a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
 		// Print link
 		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
 		// Visit link found on page
 		// Only those links are visited which are in AllowedDomains
-		c.Visit(e.Request.AbsoluteURL(link))
+		for i := range strings{
+			strings[i] = link;
+		} 
 	})
 
 	// Before making a request print "Visiting ..."
@@ -30,4 +34,5 @@ func main() {
 
 	// Start scraping on https://hackerspaces.org
 	c.Visit("https://en.wikipedia.org/wiki/Game_of_Thrones")
+	fmt.Println(strings)
 }
